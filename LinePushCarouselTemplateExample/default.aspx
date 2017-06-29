96d0d5b4bfa03ad3cb22221af0497088e039fc20
isdaviddong <isdaviddong@hotmail.com>
2017-06-29 08:00:47 +0800

    Line bot CarouselTemplate範例

---
+++ LinePushCarouselTemplateExample/default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="LinePushCarouselTemplateExample._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Channel Access Token:
+        <br />
+        <asp:TextBox ID="txbToken" runat="server" TextMode="MultiLine" Width="526px"></asp:TextBox>
+        <br />
+        <br />
+        to User :<br />
+&nbsp;<asp:TextBox ID="TxbUserId" runat="server" Width="517px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="發送" />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ LinePushCarouselTemplateExample/default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinePushCarouselTemplateExample
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //建立Bot instance
+            isRock.LineBot.Bot bot =
+                new isRock.LineBot.Bot(txbToken.Text);  //傳入Channel access token
+
+            //建立actions，作為ButtonTemplate的用戶回覆行為
+            var actions = new List<isRock.LineBot.TemplateActionBase>();
+            actions.Add(new isRock.LineBot.MessageActon() { label = "標題-文字回覆", text = "回覆文字" });
+            actions.Add(new isRock.LineBot.UriActon() { label = "標題-Google", uri = new Uri("http://www.google.com") });
+            actions.Add(new isRock.LineBot.PostbackActon() { label = "標題-發生postack", data = "abc=aaa&def=111" });
+
+            //單一Column 
+            var Column = new isRock.LineBot.Column
+            {
+                text = "ButtonsTemplate文字訊息",
+                title = "ButtonsTemplate標題",
+                //設定圖片
+                thumbnailImageUrl = new Uri("https://arock.blob.core.windows.net/blogdata201706/22-124357-ad3c87d6-b9cc-488a-8150-1c2fe642d237.png"),
+                actions = actions //設定回覆動作
+            };
+
+            //建立CarouselTemplate
+            var CarouselTemplate = new isRock.LineBot.CarouselTemplate();
+
+            //這是範例，所以用一組樣板建立三個
+            CarouselTemplate.columns.Add(Column);
+            CarouselTemplate.columns.Add(Column);
+            CarouselTemplate.columns.Add(Column);
+            //發送 CarouselTemplate
+            bot.PushMessage(this.TxbUserId.Text, CarouselTemplate);
+        }
+    }
+}
